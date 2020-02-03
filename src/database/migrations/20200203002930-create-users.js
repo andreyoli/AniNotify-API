3a83dc711e3aa862bd9535f4ca73008c3922b510
Andrey <andrey.o.paula@gmail.com>
2020-02-02 21:55:45 -0300

    Created users migration

---
+++ src/database/migrations/20200203002930-create-users.js
@@ -0,0 +1,49 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      animes: {
+        type: Sequelize.JSON
+      },
+      role_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      date_birth: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users')
+  }
+}
